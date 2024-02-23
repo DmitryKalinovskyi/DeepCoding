4491ade93cd55346a9245e0eae53ea5c53dd0a5e
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-02-23 21:39:47 +0200

    i don't remember what i did before yesterday, but this commit contains some view changes :)

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,9 +1,6 @@
-<footer class="d-flex fixed-bottom sticky-footer flex-wrap justify-content-between align-items-center py-3 bg-body-tertiary">
+<footer class="footer d-flex flex-wrap mt-5 justify-content-between align-items-center py-3 bg-body-tertiary">
     <div class="container">
         <div class="col-md-4 d-flex align-items-center">
-            <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-            </a>
             <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Dmytro Kalinovskyi</span>
             <div class="vr mx-2"></div>
             <div><a href="/views/info.php">info page</a></div>
--- views/profile.php
+++ views/profile.php
@@ -5,9 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <?php
+    include 'partials/styleincludes.php'
+    ?>
 
     <!--plotly-->
     <script src='https://cdn.plot.ly/plotly-2.29.1.min.js'></script>
@@ -27,7 +27,7 @@ function GetStars($count): string{
 $USER_TITLE = "RED"
 ?>
 
-<div class="container">
+<div class="container mt-4">
     <div class="row gx-5">
         <div class="col-4">
             <div class="bg-body-tertiary p-3 rounded-2 shadow-sm">
@@ -80,15 +80,28 @@ $USER_TITLE = "RED"
                         Activity
                     </div>
                     <div>
+                        here will be github styled day by day amount of submissions
+                    </div>
+                </div>
+                <div class="col12 bg-body-tertiary p-3 rounded-2 shadow-sm">
+                    <div>
+                        Last submissions
+                    </div>
+                    <div>
+                        <?php
 
+                        echo "<ul class='list-group'>";
+                        for($i = 0; $i < 10; $i++){
+                            echo "<li class='list-group-item'> Task {$i}</li>";
+                        }
+                        echo "</ul>";
+                        ?>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 </div>
-
 
 <?php
 include 'partials/footer.php';
