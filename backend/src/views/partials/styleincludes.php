98d84377f95952984c67b921e6d4598e72919770
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-05-25 18:36:50 +0300

    Removed bootstrap from public folder

--- backend/src/views/partials/styleincludes.php
+++ backend/src/views/partials/styleincludes.php
@@ -1,6 +1,7 @@
-<link rel="stylesheet" href="/public/style/main.css">
+<!--<link rel="stylesheet" href="/public/style/main.css">-->
+<link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
 <!--<link href="../../public/bootstrap/dist/css/bootstrap.css" rel="stylesheet">-->
-<script src="../../public/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="/public/style/style.css">
